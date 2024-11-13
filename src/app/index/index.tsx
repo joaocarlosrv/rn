e5dff975188a178108fb5c0c5c6800f71883716a
joaocarlosrv <joaocarlosrv1@gmail.com>
2024-11-13 19:57:27 -0300

    commit inicial

---
+++ src/app/index/index.tsx
@@ -0,0 +1,27 @@
+import { Image, View, TouchableOpacity } from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
+
+import { styles } from "./styles"
+import { colors } from "../styles/colors"
+import { Link } from "@/components/link"
+import { Categories } from "@/components/categories"
+
+export default function Index() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={require("@/assets/logo.png")} style={styles.logo} />
+                <TouchableOpacity>
+                    <MaterialIcons name="add" size={32} color={colors.green[300]} />    
+                </TouchableOpacity>     
+            </View>  
+
+            <Categories />
+            
+            <Link name="Rocketseat" url="https://rocketseat.com.br"
+            onDetails={() => console.log("Clicou!")} 
+            />
+        </View>   
+    )
+}
+
