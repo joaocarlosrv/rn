cd242379cdb2a2729fd20ee356107432569b317b
joaocarlosrv <joaocarlosrv1@gmail.com>
2024-11-13 22:40:08 -0300

    feat: modal

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,4 +1,4 @@
-import { Image, View, TouchableOpacity } from "react-native"
+import { Image, View, TouchableOpacity, FlatList, Modal, Text } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
 
 import { styles } from "./styles"
@@ -18,9 +18,39 @@ export default function Index() {
 
             <Categories />
             
-            <Link name="Rocketseat" url="https://rocketseat.com.br"
-            onDetails={() => console.log("Clicou!")} 
+            <FlatList
+                data={["1", "2", "3"]}
+                keyExtractor={item => item}
+                renderItem={() => (
+                    <Link 
+                        name="Rocketseat" 
+                        url="https://rocketseat.com.br"
+                        onDetails={() => console.log("Clicou!")} 
+                    />
+                )}
+                style={styles.links}
+                contentContainerStyle={styles.linksContent}
+                showsVerticalScrollIndicator={false}
             />
+            <Modal transparent visible={true}>
+                <View style={styles.modal}>
+                    <View style={styles.modalContent}>
+                        <View style={styles.modalHeader}>
+                            <Text style={styles.modalCategory}>Curso</Text>
+
+                            <TouchableOpacity>
+                                <MaterialIcons 
+                                    name="close" 
+                                    size={20} 
+                                    color={colors.gray[400]} 
+                                />
+                            </TouchableOpacity>    
+                        </View>
+                        <Text style={styles.modalLinkName}>Rocketseat</Text>
+                        <Text style={styles.modalUrl}>https://rocketseat.com.br/</Text>
+                    </View>
+                </View>
+            </Modal>
         </View>   
     )
 }
